8e715aa53afd99bbd77233d465c3aed7de6a4bc3
MaruFelis <amaruse123@gmail.com>
2023-11-23 13:51:26 -0300

    Docker File y compose

---
+++ database/init.sql
@@ -0,0 +1,19 @@
+-- to create a new database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS customersdb;
+
+-- to use database
+USE customersdb;
+
+-- creating a new table if it doesn't exist
+CREATE TABLE IF NOT EXISTS customer (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  phone VARCHAR(15)
+);
+docker
+-- to show all tables
+SHOW TABLES;
+
+-- to describe table
+DESCRIBE customer;
--- src/db.js
+++ src/db.js
@@ -1,9 +1,10 @@
 import { createPool } from "mysql2/promise";
-
+import { config } from 'dotenv';
+config()
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "holamundo",
-  port: 3306,
-  database: "customersdb",
+    port: 3306,
+    host: process.env.MYSQLDB_HOST,
+    user: process.env.MYSQLDB_USER,
+    password: process.env.MYSQLDB_PASSWORD,
+    database: process.env.MYSQLDB_DATABASE,
 });
